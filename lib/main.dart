c2f58a7375843398aeeb8e542c7d2e64afcac3d0
nabeel-khanjee <nabeelshakeel966@gmail.com>
2020-11-05 19:35:53 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "My App",
+      home: new HomePage(),
+      theme: new ThemeData(
+        accentColor: Colors.red,
+        brightness: Brightness.light,
+        primarySwatch: Colors.green,
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String mytext = "Hello World";
+
+  void _changeText(){
+    setState(() {
+      if (mytext.startsWith("H")) {
+        mytext="Welcome to my app";
+      } else {
+        mytext="Hello World";
+      }
+    });
+
+  }
+
+  Widget _bodyWidget(){
+    return new Container(
+      padding: const EdgeInsets.all(8.0),
+      child: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget> [
+            new Text(mytext, style: TextStyle(
+              fontSize: 22.2,
+            ),)
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Home Page"),
+      ),
+      body: _bodyWidget(),
+      floatingActionButton: new FloatingActionButton(
+        child: new Icon(Icons.add),
+        onPressed: _changeText,
+      ),
+    );
+  }
+}
